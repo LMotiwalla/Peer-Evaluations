ab74c0707f1ec21e3b9c388ed0059a1703d413d9
Roshan Ravi <roshan@roshanravi.com>
2021-04-22 22:30:19 -0400

    Update Comments

--- members.js
+++ members.js
@@ -1,3 +1,11 @@
+/* Expects (in sequence):
+ * - Team Number
+ * - Student Email
+ * - Student First Name
+ * - Student Last Name
+ * - Class Code
+ */
+
 const memberCSVData = 
 `Team_1, test_user1@student.uml.edu, Test, User1, 101
 Team_1, test_user2@student.uml.edu, Test, User2, 101`;
