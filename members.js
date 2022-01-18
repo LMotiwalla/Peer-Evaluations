fb4781f78441f206832967cc3323d6d599bd025c
Roshan Ravi <roshan@roshanravi.com>
2022-01-18 15:20:01 -0500

    Add Multiple Classes for Testing

--- members.js
+++ members.js
@@ -6,4 +6,6 @@
 
 const memberCSVData = 
 `MIST.6010-101,Team_1,test_user1@student.uml.edu
-MIST.6010-101,Team_1,test_user2@student.uml.edu`
+MIST.6010-101,Team_1,test_user2@student.uml.edu
+MIST.6010-102,Team_1,test_user3@student.uml.edu
+MIST.6010-102,Team_1,test_user4@student.uml.edu`
